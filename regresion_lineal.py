990e41419adfa0d81ce5c6e6c35e1e2f3ce95875
stevengodev <136918219+stevengodev@users.noreply.github.com>
2024-08-23 11:56:34 -0500

    Predecir peso con regresión lineal

---
+++ regresion_lineal.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 11:51:08 2024
+
+@author: 1103739024
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Dataset para entrenar el modelo
+data = {        
+    'ESTATURA': [121, 123, 108, 118, 111, 109, 114, 103, 110, 115],
+    'PESO': [25, 22, 19, 24, 19, 18, 20, 15, 20, 21]
+}
+
+df = pd.DataFrame(data)
+x = df[['ESTATURA']]  # Variable independiente
+y = df[['PESO']]      # Variable dependiente
+
+model = LinearRegression()
+model.fit(x, y)
+
+print(f'Coeficiente m: {model.coef_[0][0]}')
+print(f'Intercepción b: {model.intercept_[0]}')
+
+estatura_nueva = np.array([[150]])
+peso_predecido = model.predict(estatura_nueva)
+print(f'el peso predecido es: (estatura_nueva[0][0]): {peso_predecido}')
